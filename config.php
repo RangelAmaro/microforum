07de78afc36c98844787b0d979398aa5f4f4455e
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2024-03-17 15:42:37 -0300

    base de dados

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "senha";
-$dbname = "nome-banco de dados";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
